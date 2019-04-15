075a6e2fbf4927ee167a5d07a32753d949e894f5
Kyle Simon <Kyle.Simon@stonybrook.edu>
2019-04-14 23:40:08 -0400

    Fixed benchmark2 folder

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,4 @@
-//import * as functions from 'firebase-functions';
+// import * as functions from 'firebase-functions';
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,3 +7,22 @@
 //  response.send("Hello from Firebase!");
 // });
 
+// interface Player {
+//     name: String,
+//     level: number,
+//     isCat: boolean,
+//     x: number,
+//     y: number,
+//     vx: number,
+//     vy: number,
+//     timeStamp: number
+// }
+
+// interface Room {
+//     catPlayer: Player,
+//     dogPlayer: Player
+// }
+
+// exports.playerCreated = functions.firestore.document('players/{playerId}').onCreate((snap, context) => {
+
+// });
