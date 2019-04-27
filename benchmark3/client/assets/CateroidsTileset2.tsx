266babcee2c89f5018dbb576f96f25cbf0f274e1
ae <aespinal2800@gmail.com>
2019-04-26 23:06:02 -0400

    Level 4 added

--- benchmark3/client/assets/CateroidsTileset2.tsx
+++ benchmark3/client/assets/CateroidsTileset2.tsx
@@ -3,73 +3,81 @@
  <image source="CateroidsTileset2.png" width="320" height="128"/>
  <tile id="0" type="asteroid">
   <properties>
-   <property name="VelocityX" type="float" value="0"/>
-   <property name="VelocityY" type="float" value="0"/>
-   <property name="asteroid0Damage" type="float" value="20"/>
-   <property name="asteroid0Health" type="float" value="40"/>
+   <property name="damage" type="float" value="20"/>
+   <property name="health" type="float" value="40"/>
+   <property name="level" type="int" value="0"/>
+   <property name="velocityX" type="float" value="0"/>
+   <property name="velocityY" type="float" value="0"/>
   </properties>
  </tile>
  <tile id="1" type="asteroid">
   <properties>
-   <property name="VelocityX" type="float" value="0"/>
-   <property name="VelocityY" type="float" value="0"/>
-   <property name="asteroid1Damage" type="float" value="15"/>
-   <property name="asteroid1Health" type="float" value="20"/>
+   <property name="damage" type="float" value="15"/>
+   <property name="health" type="float" value="20"/>
+   <property name="level" type="int" value="1"/>
+   <property name="velocityX" type="float" value="0"/>
+   <property name="velocityY" type="float" value="0"/>
   </properties>
  </tile>
  <tile id="2" type="asteroid">
   <properties>
-   <property name="VelocityX" type="float" value="0"/>
-   <property name="VelocityY" type="float" value="0"/>
-   <property name="asteroid2Damage" type="float" value="10"/>
-   <property name="asteroid2Health" type="float" value="10"/>
+   <property name="damage" type="float" value="10"/>
+   <property name="health" type="float" value="10"/>
+   <property name="level" type="int" value="2"/>
+   <property name="velocityX" type="float" value="0"/>
+   <property name="velocityY" type="float" value="0"/>
   </properties>
  </tile>
  <tile id="3" type="asteroid">
   <properties>
-   <property name="VelocityX" type="int" value="0"/>
-   <property name="VelocityY" type="int" value="0"/>
-   <property name="asteroid3Damage" type="float" value="500"/>
-   <property name="asteroid3Health" type="float" value="100"/>
+   <property name="damage" type="float" value="500"/>
+   <property name="health" type="float" value="300"/>
+   <property name="level" type="int" value="3"/>
    <property name="oxygenReplenishDelay" type="int" value="50"/>
    <property name="oxygenReplenishRate" type="float" value="0.15"/>
+   <property name="velocityX" type="int" value="0"/>
+   <property name="velocityY" type="int" value="0"/>
   </properties>
  </tile>
  <tile id="4" type="laser">
   <properties>
-   <property name="Type" value="VERTICAL"/>
    <property name="laserDamage" type="float" value="1"/>
    <property name="laserDelay" type="int" value="50"/>
    <property name="laserDuration" type="int" value="2500"/>
    <property name="laserSprites" type="int" value="50"/>
    <property name="playerDeltaX" type="float" value="200"/>
+   <property name="scale" type="float" value="4"/>
+   <property name="type" value="VERTICAL"/>
   </properties>
  </tile>
  <tile id="5" type="dog">
   <properties>
-   <property name="VelocityX" type="float" value="0"/>
-   <property name="VelocityY" type="float" value="0"/>
-   <property name="dog3Damage" type="float" value="15"/>
-   <property name="dog3FireRate" type="int" value="1500"/>
-   <property name="dog3Health" type="float" value="80"/>
+   <property name="damage" type="float" value="15"/>
+   <property name="fireRate" type="float" value="1500"/>
+   <property name="health" type="float" value="80"/>
+   <property name="level" type="int" value="3"/>
+   <property name="velocityX" type="float" value="0"/>
+   <property name="velocityY" type="float" value="0"/>
   </properties>
  </tile>
  <tile id="6" type="dog">
   <properties>
-   <property name="VelocityX" type="float" value="0"/>
-   <property name="VelocityY" type="float" value="0"/>
-   <property name="dog2Damage" type="float" value="10"/>
-   <property name="dog2FireRate" type="int" value="1000"/>
-   <property name="dog2Health" type="float" value="60"/>
+   <property name="damage" type="float" value="10"/>
+   <property name="fireRate" type="float" value="1000"/>
+   <property name="health" type="float" value="60"/>
+   <property name="level" type="int" value="2"/>
+   <property name="velocityX" type="float" value="0"/>
+   <property name="velocityY" type="float" value="0"/>
   </properties>
  </tile>
  <tile id="7" type="dog">
   <properties>
-   <property name="VelocityX" type="float" value="0"/>
-   <property name="VelocityY" type="float" value="0"/>
-   <property name="dog1Damage" type="float" value="15"/>
-   <property name="dog1FireRate" type="int" value="800"/>
-   <property name="dog1Health" type="float" value="30"/>
+   <property name="damage" type="float" value="5"/>
+   <property name="fireRate" type="float" value="800"/>
+   <property name="health" type="float" value="30"/>
+   <property name="level" type="int" value="1"/>
+   <property name="velocityX" type="float" value="0"/>
+   <property name="velocityY" type="float" value="0"/>
   </properties>
  </tile>
  <tile id="8" type="spawnPoint"/>
