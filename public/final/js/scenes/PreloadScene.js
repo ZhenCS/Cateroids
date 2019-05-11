0505fd17fe7186dc12f1726456f964e7f3e02229
zhenlu <zheng.lu@stonybrook.edu>
2019-05-10 21:05:27 -0400

    Added dog weapon sprite

--- public/final/js/scenes/PreloadScene.js
+++ public/final/js/scenes/PreloadScene.js
@@ -57,6 +57,7 @@ export class LoadScene extends Phaser.Scene {
     this.load.image(constants.BOSSBEAMKEY, 'assets/bossBeam.png');
     this.load.image(constants.MOONKEY, 'assets/moon.png');
     this.load.image(constants.DOGWALLKEY, 'assets/dogWall2.png');
+    this.load.image(constants.DOGWALLWEAPONKEY, 'assets/bossWeapon.png');
     this.load.image(constants.ENDPOINT, 'assets/endPoint.png');
 
     this.load.image(constants.CONTROLS1KEY, 'assets/controls1.png');
